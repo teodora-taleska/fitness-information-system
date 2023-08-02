a8d983a810444a002fb995fdbc312a9231a98c7c
Teodora <taleska000@gmail.com>
2023-08-02 20:51:52 +0200

    All necessary pages created

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import {
+  createBrowserRouter, 
+  RouterProvider,
+  Route,
+
+} from "react-router-dom"
+
+const Layout = () => {
+  return(
+    <>
+    </>
+  )
+  }
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <div>Hello world!!! This is home</div>
+  },
+  {
+    path:"/test",
+    element: <Layout />
+  }
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <div className="container">
+        <p>Hello world</p>
+        <div>
+          <RouterProvider router={router}/>
+        </div>
+        </div>
+      </div>
+      
   );
 }
 
