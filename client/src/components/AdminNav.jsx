8ceef8c879bb4afc202a5533af4e3d34e895a7b6
Teodora <taleska000@gmail.com>
2023-08-05 20:10:21 +0200

    Admin nav bar created

--- client/src/components/AdminNav.jsx
+++ client/src/components/AdminNav.jsx
@@ -1,23 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import { FiLogOut } from 'react-icons/fi'
 
 const AdminNav = () => {
+    
+
     return(
         <div className="admin-nav">
-            <div class="nav-header">
+            <div className="nav-header">
                 <h1>Admin Panel</h1>
             </div>
-            <ul class="nav-list">
-                <li class="nav-item">
-                <a href="#">Pages</a>
-                <ul class="dropdown">
-                    <li><a href="#">Edit Pages</a></li>
-                    <li><a href="#">Add Employee Account</a></li>
-                    <li><a href="#">Modify Employee Account</a></li>
-                    <li><a href="#">Delete Employee Account</a></li>
-                </ul>
+            <ul className="nav-list">
+                <li className="nav-item"
+                >
+                    <a href="#">Edit pages</a>
+                    <ul className="dropdown">
+                        <li><Link to="/admin">Home</Link></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Activities</a></li>
+                        <li><a href="#">Events</a></li>
+                        <li><a href="#">Shop</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Register</a></li>
+                    </ul>
+
                 </li>
+
+                <li className="nav-item"
+              >
+                    <a href="#" >Manage employees</a>
+                    <ul className="dropdown">
+                        <li><Link to="/admin/add-employee">Add employee account</Link></li>
+                        <li><Link to="/admin/modify-employee">Modify employee account</Link></li>
+                        <li><Link to="/admin/delete-employee">Delete employee account</Link></li>
+
+                    </ul>
+                
+
+                </li>
+
                 
             </ul>
+
+            <Link to="/"><div className="logout-button">
+                <FiLogOut />    
+            </div></Link>
         </div>
     )
 }
