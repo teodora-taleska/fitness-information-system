38df3df0c8ce4819632c0d3c85281222945d3481
Teodora <taleska000@gmail.com>
2023-08-09 19:16:22 +0200

    CSS changes for the shop page

--- client/src/pages/EditHome.jsx
+++ client/src/pages/EditHome.jsx
@@ -3,7 +3,23 @@ import React from "react";
 const EditHome = () => {
     return(
         <div className="edit-home">
-           
+           <section className="header">
+
+                <div className="content">
+                    <h2>BUILD <span>YOUR</span> BODY <br/>
+                TRANSFORM <span>YOUR</span> MIND</h2>
+
+                    <p>Are you ready to embark on a journey towards a stronger, fitter, and healthier version of yourself? Look no further than FusionFit. <br/>Our state-of-the-art fitness facility is designed to be your ultimate destination for achieving your wellness goals and transforming your lifestyle.</p>
+                
+                    <button >CONTACT</button>
+                    <button >READ MORE</button>
+
+                </div>
+
+            </section>
+            <footer className="footer">
+                <p>&copy; 2023 FusionFit Fitness Center. All rights reserved.</p>
+            </footer>
         </div>
     )
 }
