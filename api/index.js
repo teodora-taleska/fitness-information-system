5a4392c272af093eb8203ec19dd146cd8e669b61
teodora <taleska000@gmail.com>
2023-08-14 20:35:28 +0200

    Authentication process

--- api/index.js
+++ api/index.js
@@ -1,16 +1,19 @@
 import express from "express";
+import cors from "cors";
 
 import dotenv from "dotenv";
 dotenv.config({path: '.env'}); //Load the enviroment variables from .env file
 
 import mysql from "mysql2"
 
+import authRoutes from "./routes/auth.js"
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 
-const db = mysql.createConnection({
+export const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -22,11 +25,14 @@ db.connect((err, connecttion) => {
       console.log("ERROR: "+ err.message)
       return
   }
-  console.log("Connection established!")
+  console.log("Database connected!")
   // connection.release()
 })
 
+// Routes
+app.use("/api/auth/", authRoutes)
 
-app.listen(5064, () => {
-    console.log("Connected!")
+// const port = 5000
+app.listen(5066, () => {
+    console.log("Server connected on port!:" + 5066)
 })
