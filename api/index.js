3ddae5a499d296bb47ed31fc2c96fb5cb3307b9d
Teodora <taleska000@gmail.com>
2023-08-14 20:49:51 +0200

    Solved index.js conflicts

--- api/index.js
+++ api/index.js
@@ -20,19 +20,6 @@ export const db = mysql.createPool({
     database:process.env.DB_NAME,
 })
 
-export const db = mysql.createPool({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME,
-})
-
-export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-})
 
 db.connect((err, connecttion) => {
   if (err) {
